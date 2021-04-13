c1606b3bd3fb0b61c2db66be0faaef2ee53b095d
zypang <pangzaiyu@163.com>
2021-04-13 10:35:18 +0800

    add ypppt crawler

--- YppptCrawler.py
+++ YppptCrawler.py
@@ -1,7 +1,7 @@
 from functools import partial
 
 import lxml.html
-from utils import fetch_html,decode_html,url_refine,fetch_binary
+from utils import fetch_html,url_refine,fetch_binary
 
 class YppptCrawler:
     base_url="https://www.ypppt.com"
@@ -34,19 +34,37 @@ class YppptCrawler:
         doc=lxml.html.fromstring(fetch_html(url))
         ul=doc.cssselect("ul.down")[0]
         down_urls=ul.xpath("li/a/@href")
-        item['down_url']=down_urls
+        item['down_url']=[self.yp_url(x) for x in down_urls]
 
-    def download_file(self,name,url):
+    def download_file(self,path,url):
         b=fetch_binary(url)
-        with open(name+".zip",'wb') as file:
+        with open(path,'wb') as file:
             file.write(b)
 
 if __name__ == '__main__':
     c=YppptCrawler()
-    items=c.get_ppt_list("jianyue",2)
-    print(items)
-    #c.get_down_url(items[0])
-    #c.download_file(items[0]['name'],items[0]['down_url'][0])
+    # max page: 143
+    sum=0
+    failed_items=[]
+    for i in range(14,51):
+        print(f"get ppt list in page {i}...")
+        items=c.get_ppt_list("",i)
+        for item in items:
+            print(f"get download urls of {item['name']} ...")
+            try:
+                c.get_down_url(item)
+                print(f"download ppt {item['name']} in page {i}...")
+                c.download_file(f"ppts/{item['name']}.zip",item['down_url'][0])
+            except:
+                print(f"!!! download failed: {item['url']}")
+                failed_items.append(item)
+            #else:
+            #    print(f"download finished")
+        sum+=len(items)
+    print(f"done! Totally downloaded {sum} ppts")
+    print("failed items:")
+    print(failed_items)
+    print(f"failed item count: {len(failed_items)}")
 
 
 
