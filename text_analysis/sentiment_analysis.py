23614df2a01424c419328ced3d7b8a0e0aa15112
zypang <pangzaiyu@163.com>
2021-04-20 11:38:45 +0800

    fix predict problem. Add word count

--- text_analysis/sentiment_analysis.py
+++ text_analysis/sentiment_analysis.py
@@ -29,11 +29,20 @@ def split_data(pos_data,neg_data):
     X=pos_data+neg_data
     y=[True]*len(pos_data)+[False]*len(neg_data)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=33)
-    return (X_train,y_train),(X_test,y_test)
+    return X_train,y_train,X_test,y_test
+
+def word_split(X,cut_method='paddle'):
+    if cut_method == 'paddle':
+        results = word_cut.paddle_cut(X)
+    else:
+        results = word_cut.jieba_cut(X)
+    return [res['word'] for res in results]
+
 
 class SentimentAnalysis:
-    feature_number=150
-    def __init__(self,vec_method="TW",cut_method='paddle',stop_words=()):
+    #feature_number=400
+    feature_number=100
+    def __init__(self,vec_method="TW",stop_words=()):
         '''
         构造函数
         :param vec_method: 向量化方法: TW, TC, TF, TF-IDF
@@ -42,7 +51,7 @@ class SentimentAnalysis:
         '''
         self.__vec_method=vec_method
         self.__stop_words=stop_words
-        self.__cut_method=cut_method
+        #self.__cut_method=cut_method
 
         #if pos_data and neg_data:
         #    self.load_data(pos_data,neg_data)
@@ -64,29 +73,23 @@ class SentimentAnalysis:
     '''
 
 
-    def __word_cut(self,X):
-        if self.__cut_method == 'paddle':
-            results=word_cut.paddle_cut(X)
-        else:
-            results=word_cut.jieba_cut(X)
-        return [' '.join(res['word']) for res in results]
-
     def __vectorize(self,X):
-        X=self.__word_cut(X)
+        X=[' '.join(words) for words in X]
         self.__vectorizer=self.__get_vectorizer()
         return self.__vectorizer.fit_transform(X)
 
     def __feature_selection(self,X,y):
         X=self.__vectorize(X)
         self.__feature_selector=SelectKBest(chi2, k=self.feature_number)
-        X_new = self.__feature_selector.fit_transform(X, y)
-        return X_new
+        X= self.__feature_selector.fit_transform(X, y)
+        return X
 
     def train(self, X, y):
         X=self.__feature_selection(X,y)
         self.__clf=NuSVC(nu=0.4,kernel='rbf').fit(X,y)
 
     def predict(self,X):
+        X=[' '.join(words) for words in X]
         X=self.__vectorizer.transform(X)
         X=self.__feature_selector.transform(X)
         return self.__clf.predict(X)
@@ -96,10 +99,13 @@ if __name__ == '__main__':
     #print(read_stop_words("data/baidu_stopwords.txt")[:10])
     pos_data=read_comment_json("data/jdcmts_good.json")
     neg_data = read_comment_json("data/jdcmts_bad.json")
-    train_data,test_data=split_data(pos_data,neg_data)
+    X_train,y_train,X_test,y_test=split_data(pos_data,neg_data)
     stop_words=read_stop_words("data/baidu_stopwords.txt")
-    sa=SentimentAnalysis(vec_method='TF-IDF',stop_words=stop_words)
-    sa.train(train_data[0],train_data[1])
-    pred_y=sa.predict(test_data[0])
-    print(accuracy_score(test_data[1],pred_y))
+    X_train=word_split(X_train)
+    X_test=word_split(X_test)
+    #print(word_cut.word_count(X_train[:10]))
+    sa=SentimentAnalysis(vec_method='TW',stop_words=stop_words)
+    sa.train(X_train,y_train)
+    pred_y=sa.predict(X_test)
+    print(accuracy_score(y_test,pred_y))
 
--- text_analysis/word_cut.py
+++ text_analysis/word_cut.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import jieba
 import paddlehub as hub
 
@@ -14,6 +16,13 @@ def paddle_cut(text_list,tag=False):
     lac = hub.Module(name="lac")
     results = lac.cut(text=text_list, use_gpu=False, batch_size=1, return_tag=tag)
     return results
+
+def word_count(text_list):
+    wc=defaultdict(int)
+    for words in text_list:
+        for word in words:
+            wc[word]+=1
+    return wc
 
 if __name__ == '__main__':
     text=["今天是个好天气"]
